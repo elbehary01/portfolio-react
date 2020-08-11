1e3b5785827187ca58b16999c6527336d4e7609c
osman190 <ahmedelbehary0@gmail.com>
2020-08-11 11:49:41 +0200

    add bootstrap as CDN in HTML

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
