f70dd3bac625782e432ac677732fbf27721a1ee2
elbehary01 <29628969+elbehary01@users.noreply.github.com>
2026-04-30 11:54:47 +0200

    server.js was missing

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
+import connectDB from './src/config/mongodb.js';
+
+import contactRoute from './src/routes/contactRoute.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// App Config
+const app = express();
+const port = process.env.PORT || 9000;
+connectDB();
+
+// Middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// api endpoints
+app.use('/api/contact', contactRoute);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static(path.join(__dirname, '../client/public')));
+  // console.log(process.env.NODE_ENV);
+
+  app.get('/', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('API is running...'));
+}
+
+app.listen(port, () => {
+  console.log('Server started on PORT: ' + port);
+});
